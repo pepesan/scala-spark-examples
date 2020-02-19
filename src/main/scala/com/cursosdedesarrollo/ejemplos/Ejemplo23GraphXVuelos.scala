a7bd26e22ad03d6d5d59c2b36eab66cfbff066a0
pepesan <pepesan@gmail.com>
2020-02-19 03:33:24 +0100

    mejroas en el Ejemplo23GraphXVuelos

--- src/main/scala/com/cursosdedesarrollo/ejemplos/Ejemplo23GraphXVuelos.scala
+++ src/main/scala/com/cursosdedesarrollo/ejemplos/Ejemplo23GraphXVuelos.scala
@@ -49,7 +49,7 @@ object Ejemplo23GraphXVuelos {
     // filtro los vuelos en los que hemos tenido excepción
     val flightsRDDfiltered = flightsRDD.filter(_.arrdelay==0)
     val airports = flightsRDDfiltered.map(flight => (flight.org_id, flight.origin)).distinct
-    airports.take(1)
+    airports.take(1).foreach(println)
 
     // Defining a default vertex called nowhere
     val nowhere = "nowhere"
@@ -72,14 +72,15 @@ object Ejemplo23GraphXVuelos {
     // LNumber of airports
     val numairports = graph.numVertices
     println("Numero de vértices: "+ numairports)
-    println("Ejemplos de vértices: "+ graph.vertices.take(2))
+    println("Ejemplos de vértices: ")
+    graph.vertices.take(2).foreach(println)
     // Number of routes
     val numroutes = graph.numEdges
     println("Numero de rutas: "+ numroutes)
-    println("Ejemplos de rutas: "+ graph.edges.take(2))
+    println("Ejemplos de rutas: ")
+    graph.edges.take(2).foreach(println)
     // filtramos los que tengan una distancia superior a 1000
-    val mayoresmil=graph.edges.filter { case (Edge(org_id, dest_id, distance)) => distance > 1000 }.take(3)
-    println(mayoresmil)
+    val mayoresmil=graph.edges.filter { case (Edge(org_id, dest_id, distance)) => distance > 1000 }.take(3).foreach(println)
     // res9: Array[org.apache.spark.graphx.Edge[Int]] = Array(Edge(10140,10397,1269), Edge(10140,10821,1670), Edge(10140,12264,1628))
 
     // imprimimos los tres primeros tripletes
@@ -103,7 +104,7 @@ object Ejemplo23GraphXVuelos {
     // which airport has the most incoming flights?
     graph.inDegrees.collect.sortWith(_._2 > _._2).map(x => (airportMap(x._1), x._2))
     //res46: Array[(String, Int)] = Array((ATL,152), (ORD,145), (DFW,143), (DEN,132), (IAH,107), (MSP,96), (LAX,82), (EWR,82), (DTW,81), (SLC,80),
-    graph.outDegrees.join(airports).sortBy(_._2._1, ascending = false).take(1)
+    graph.outDegrees.join(airports).sortBy(_._2._1, ascending = false).take(1).foreach(println)
     val maxout = graph.outDegrees.join(airports).sortBy(_._2._1, ascending = false).take(3)
     //res46: Array[(String, Int)] = Array((ATL,152), (ORD,145), (DFW,143), (DEN,132), (IAH,107), (MSP,96), (LAX,82), (EWR,82), (DTW,81), (SLC,80),
     val maxIncoming = graph.inDegrees.collect.sortWith(_._2 > _._2).map(x => (airportMap(x._1), x._2)).take(3)
@@ -118,11 +119,11 @@ object Ejemplo23GraphXVuelos {
 
     // What are the top 10 flights from airport to airport?
     graph.triplets.sortBy(_.attr, ascending = false).map(triplet =>
-      "There were " + triplet.attr.toString + " flights from " + triplet.srcAttr + " to " + triplet.dstAttr + ".").take(10)
+      "There were " + triplet.attr.toString + " flights from " + triplet.srcAttr + " to " + triplet.dstAttr + ".").take(10).foreach(println)
 
     val sourceId: VertexId = 13024
     // 50 + distance / 20
-    graph.edges.filter { case (Edge(org_id, dest_id, distance)) => distance > 1000 }.take(3)
+    graph.edges.filter { case (Edge(org_id, dest_id, distance)) => distance > 1000 }.take(3).foreach(println)
 
     val gg = graph.mapEdges(e => 50.toDouble + e.attr.toDouble / 20)
     val initialGraph = gg.mapVertices((id, _) => if (id == sourceId) 0.0 else Double.PositiveInfinity)
