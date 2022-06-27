65692fdbbfcc81f5d088fa71fb1e8f4260329949
David <dvaquero@bilbomatica.es>
2022-06-27 09:18:46 +0200

    Quitados ejemplos no funcionales en 2.13

--- src/main/scala/com/cursosdedesarrollo/ejemplos/Ejemplo10BrezeViz.scala
+++ src/main/scala/com/cursosdedesarrollo/ejemplos/Ejemplo10BrezeViz.scala
@@ -35,8 +35,8 @@ object Ejemplo10BrezeViz {
     val f = Figure()
     val p = f.subplot(0)
     val x = linspace(0.0,1.0)
-    p += plot(x, x :^ 2.0)
-    p += plot(x, x :^ 3.0, '.')
+    p += plot(x, x ^:^ 2.0)
+    p += plot(x, x ^:^ 3.0, '.')
     p.xlabel = "x axis"
     p.ylabel = "y axis"
     f.saveas("salidas/lines.png") // save current figure as a .png, eps and pdf also supported
